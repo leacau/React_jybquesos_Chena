66843d0460afa66d2000beaae1759a6ccc88fa01
leacau <leandrochena@gmail.com>
2022-05-31 19:53:28 -0300

    limpiando el código

--- src/App.js
+++ src/App.js
@@ -7,21 +7,9 @@ import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <NavBar />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="Parrafo">
-          <ItemListContainer />
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <ItemListContainer />
+    
     </div>
   );
 }
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,25 +3,27 @@ import CartWidget from "../CartWidget/CartWidget"
 
 const NavBar = () =>{
     return (
-    <nav className='navBar'>
-        <ul className='ulNav'>
-            <li className='ilNav'>
-                <a className="navLink" href="#">Inicio</a>
-            </li>
-            <li>
-                <a className="navLink" href="#">Tienda</a>
-            </li>
-            <li>
-                <a className="navLink" href="#">Login</a>
-            </li>
-            <li>
-                <a className="navLink" href="#">Administrador</a>
-            </li>
-            <li>
-                <a className="navLink" href="#">0 <CartWidget /></a>
-            </li>
-        </ul>
-    </nav>    
+    <header className="App-header">
+        <nav className='navBar'>
+            <ul className='ulNav'>
+                <li className='ilNav'>
+                    <a className="navLink" href="#">Inicio</a>
+                </li>
+                <li>
+                    <a className="navLink" href="#">Tienda</a>
+                </li>
+                <li>
+                    <a className="navLink" href="#">Login</a>
+                </li>
+                <li>
+                    <a className="navLink" href="#">Administrador</a>
+                </li>
+                <li>
+                    <a className="navLink" href="#">0 <CartWidget /></a>
+                </li>
+            </ul>
+        </nav>
+    </header>
 )}
 
 export default NavBar
